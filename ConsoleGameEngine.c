6cae3c3cde61bf472d86f10234d0cfbf5f73d145
joaolavaqui <145854564+joaolavaqui@users.noreply.github.com>
2024-05-27 22:26:55 -0300

    Update ConsoleGameEngine.c

--- ConsoleGameEngine.c
+++ ConsoleGameEngine.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <conio.h>
+#include <windows.h>
 
 #define BLACK   "\x1b[30m"
 #define RED     "\x1b[31m"
@@ -15,18 +16,35 @@
 // ENGINE                                  //
 // functions that control the entire thing //
 
+//puts the screen cursor on top left
+void rst_screen()
+{
+	COORD coord;
+	coord.X = 0;
+	coord.Y = 0;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
 //object struct
 typedef struct
 {
-    float X, Y; 
+    float X, Y;
     float Width, Height;
     float vX, vY;
     int Color;
 
 }object;
 
+//sound struct
+typedef struct 
+{
+    int frequency;
+    int duration;
+}sound;
+
 //set display
 int display[40][30]={};
+int display_color = 0;
 
 //calculate distance beetween two objects coordinates
 float distance(object a, object b)
@@ -46,13 +64,18 @@ int colide_obj(object a, object b)
     return 1;
 }
 
+//detect if a point colide with a object
+int colide_point(float x, float y, object a)
+{
+    if(x>a.X && x<(a.X+a.Width) && y>a.Y && y<(a.Y+a.Height)){return 1;}
+    return 0;
+}
+
 //simple seed based RNG functionality
-float rng_seed=0.45673;
 float rng(float lb, float ub)
 {
-    float result;
-    result = rng_seed * 3.73 * (1-rng_seed);
-    rng_seed = result;
+    float result = rand()%10000;
+    result = result/10000;
 
     result = (result*(ub-lb)) + lb;
 
@@ -106,13 +129,21 @@ object update_obj(object a)
     return a;
 }
 
+//show variables of especific object
+void debug_obj(object a)
+{
+    printf("X=%.1f vX=%.1f\n", a.X, a.vX);
+    printf("Y=%.1f vY=%.1f\n", a.Y, a.vY);
+    printf("Width=%.1f\n", a.Width);
+    printf("Height=%.1f\n", a.Height);
+}
+
 //render display array
 char render()
 {
-    system("cls");
+    rst_screen();
     for(int rY=0 ; rY<30 ; rY++)
     {
-        printf("\n");
         for(int rX=0 ; rX<40 ; rX++)
         {
             if     (display[rX][rY] == 0){printf(BLACK);}
@@ -125,20 +156,31 @@ char render()
             else if(display[rX][rY] == 7){printf(WHITE);}
 
             printf("%c%c", 219, 219);
-            display[rX][rY] = 0;
+            display[rX][rY] = display_color;
         }
+        printf("\n");
     }
+    Sleep(1);
     return 0;
 }
+
+//play sound!
+void play_sound(sound a)
+{
+    Beep(a.frequency, a.duration);
+}
+
 
 // GAME                //
 // Flappy Bird Example //
 
 int main(void)
 {
+    //Start System
+    display_color = 0;
+    system("cls");
 
     //Start Game Objects
-
     object player;
     player.X = 3;
     player.Y = 10;
@@ -160,16 +202,40 @@ int main(void)
     downpipe.Height = 30;
     downpipe.Color = 2;
 
+    //create sounds
+    sound flap;
+    flap.frequency=200;
+    flap.duration=5;
+
+    sound scored;
+    scored.frequency=2000;
+    scored.duration=20;
+
+    //Variables
+    int score=0;
+
+    //Start Menu
+    printf(GREEN);
+    printf("\n\nFLAPPY BIRD\n");
+    printf("CGE Preview\n");
+    printf(YELLOW);
+    printf("\n\nType 'W' to start game:\n");
+    while (input() != 'w'){}
+
     //Frame Update
     while(1)
     {
-        if(input() == 'w'){player.vY = -4;}
+        if(input() == 'w')
+        {
+            player.vY = -4;
+            play_sound(flap);
+        }
         player.vY += 1;
         if(player.vY > 3){player.vY = 3;}
         player = update_obj(player);
 
-        uppipe.X -= 2;
-        downpipe.X -= 2;
+        uppipe.X -= 4;
+        downpipe.X -= 4;
 
         if(uppipe.X < -2)
         {
@@ -178,17 +244,30 @@ int main(void)
 
             uppipe.Height = (float)(int)rng(0, 15);
             downpipe.Y = uppipe.Height + 15;
+            score++;
+            play_sound(scored);
         }
 
-        if(colide_obj(player, uppipe)){return 0;}
-        if(colide_obj(player, downpipe)){return 0;}
-        if(player.Y < -2){return 0;}
-        if(player.Y > 30){return 0;}
+        if(colide_obj(player, uppipe)){break;}
+        if(colide_obj(player, downpipe)){break;}
+        if(player.Y < -2){break;}
+        if(player.Y > 30){break;}
 
-        display_obj(player);
         display_obj(uppipe);
         display_obj(downpipe);
+        display_obj(player);
         render();
     }
+
+    //Game End
+    system("cls");
+    printf(RED);
+    printf("\n\nGAME OVER!\n\n");
+    printf(GREEN);
+    printf("Score:%i\n", score);
+    printf(YELLOW);
+    printf("\n\nType 'W' to close game:\n");
+    while (input() != 'w'){}
+
     return 0;
 }
